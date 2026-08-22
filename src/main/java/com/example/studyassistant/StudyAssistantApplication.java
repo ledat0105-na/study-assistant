3b31c5cdfdf388f61ed00d3ff56ad1aa939c125a
DatLNT <tle723772@gmail.com>
2026-08-22 08:31:58 +0700

    Initial Spring Boot project

---
+++ src/main/java/com/example/studyassistant/StudyAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyAssistantApplication.class, args);
+    }
+
+}
